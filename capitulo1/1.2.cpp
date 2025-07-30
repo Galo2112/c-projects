212e4f806bff5be0fae27a8450b7671d725f4a65
Galo Castelan Salas <galocastelan2112@gmail.com>
2025-07-30 11:35:37 -0600

    Create 1.2.cpp

---
+++ capitulo1/1.2.cpp
@@ -0,0 +1,12 @@
+// capitulo 1.2.1cpp 
+#include <iostream>
+
+int main()
+{
+   std::cout << "Enter two numbers:" << std::endl;
+  int v1=0,v2=0;
+  std::cin >> v1 >> v2 ;
+  std::cout << "The sum of " << V1<< " and " << v2
+    << " is " << v1 + v2 << std::endl;
+  return 0;
+}
